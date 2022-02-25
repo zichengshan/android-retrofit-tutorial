eb4d43f77debc5cafe6cddae291fa4c86e42aa35
Zicheng.S <zichengshan@gmail.com>
2022-02-24 23:03:30 -0800

    Update MovieListActivity.java

--- app/src/main/java/edu/uci/swe264p/retrofit/MovieListActivity.java
+++ app/src/main/java/edu/uci/swe264p/retrofit/MovieListActivity.java
@@ -1,14 +1,64 @@
 package edu.uci.swe264p.retrofit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class MovieListActivity extends AppCompatActivity {
+    // we use recyclerView here
+    private RecyclerView recyclerView;
 
+    static final String TAG = MainActivity.class.getSimpleName();
+    static final String BASE_URL = "https://api.themoviedb.org/3/";
+    static Retrofit retrofit = null;
+    final static String API_KEY = "fd90c83bbb9b4fb0bd9f4699b47f0d7b";
+    List<Movie> movies = null;
+
+    // Different from ProgramListActivity, we need to get info form api
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
+
+        recyclerView = findViewById(R.id.rvMovieList);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        recyclerView.setAdapter(new ProgramListAdapter(movies));
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        MovieApiService movieApiService = retrofit.create(MovieApiService.class);
+        Call<TopRatedResponse> call = movieApiService.getTopRatedMovies(API_KEY);
+
+        call.enqueue(new Callback<TopRatedResponse>() {
+            @Override
+            public void onResponse(Call<TopRatedResponse> call, Response<TopRatedResponse> response) {
+                movies = response.body().getTopMovies();
+                recyclerView.setAdapter(new MovieListAdapter(movies));
+            }
+
+            @Override
+            public void onFailure(Call<Movie> call, Throwable throwable) {
+                Log.e(TAG, throwable.toString());
+            }
+        });
     }
 }
