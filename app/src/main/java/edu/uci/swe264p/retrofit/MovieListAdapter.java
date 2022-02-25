4eb1dea0c46d2fdc5edc06b376cf20b2bb5cee82
Zicheng.S <zichengshan@gmail.com>
2022-02-24 23:18:05 -0800

    Update MovieListAdapter.java

--- app/src/main/java/edu/uci/swe264p/retrofit/MovieListAdapter.java
+++ app/src/main/java/edu/uci/swe264p/retrofit/MovieListAdapter.java
@@ -1,6 +1,60 @@
 package edu.uci.swe264p.retrofit;
 
 
-public class MovieListAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.ViewHolder> {
+    private List<Movie> movies;
+    MovieListAdapter(List<Movie> movies){
+        this.movies = movies;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView ivMovie;
+        TextView tvTitle;
+        TextView tvReleaseDate;
+        TextView tvVote;
+        TextView tvOverview;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivMovie = itemView.findViewById(R.id.ivMovie);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvReleaseDate = itemView.findViewById(R.id.tvReleaseDate);
+            tvVote = itemView.findViewById(R.id.tvVote);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+        }
+    }
+
+    @NonNull
+    @Override
+    public MovieListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieListAdapter.ViewHolder holder, int position) {
+        Movie movie = movies.get(position);
+        Picasso.get().load("https://image.tmdb.org/t/p/w500/"+movie.getPosterPath()).into(holder.ivMovie);
+        holder.tvTitle.setText(movie.getTitle());
+        holder.tvReleaseDate.setText(movie.getReleaseDate());
+        holder.tvVote.setText(String.valueOf(movie.getVoteAverage()));
+        holder.tvOverview.setText(movie.getOverview());
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
 }
